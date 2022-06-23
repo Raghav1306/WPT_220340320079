10fd66717e26de5a3093e77b4e71b55548adb72a
Raghav1306 <100940958+Raghav1306@users.noreply.github.com>
2022-06-23 12:16:29 +0530

    Add files via upload

---
+++ book.js
@@ -0,0 +1,67 @@
+let raghav=({
+    host: 'localhost',
+    user: 'root',
+    password: 'cdac',
+    database: 'raghav13',
+	port:3306
+});
+
+const mysql = require('mysql2');
+const con = mysql.createConnection(raghav);
+
+const express=require("express");
+const app=express();
+
+app.use(express.static("abc"));
+
+app.listen(8081,()=>{
+console.log("server listening at 8081");
+});
+
+app.get('/book',(req,res)=>{
+    console.log("bookid");
+    let bookid=req.query.bookid;
+
+    let output={status:false,book:{bookname:"",price:""}};
+    con.query('select bookname,price from book where bookid=?',[bookid],
+    (err,rows)=>{
+        if (err) {
+            console.log(err);
+			
+        } else {
+            if(rows.length>0)
+          output.status=true;
+          output.book.bookname=rows[0].bookname;
+          output.book.price=rows[0].price;
+            };
+            res.send(output);
+    
+    });
+			
+       
+
+});
+
+    
+    app.get('/updatebook',(req,res)=>{
+        console.log(bookid);
+        let bookdetail={bookid:req.query.bookid,bookname:req.query.bookname,price:req.query.price}
+    
+        let output={status:false};
+        con.query('update book set price=? where bookid=?',[bookdetails.price,bookdetails.bookid],
+        (err,rows)=>{
+            if (err) {
+                console.log(err);
+                
+            } else {
+                if(rows.affectedRows>0)
+              output.status=true;
+              
+                };
+                res.send(output);
+            
+            });
+                
+           
+    
+    });
